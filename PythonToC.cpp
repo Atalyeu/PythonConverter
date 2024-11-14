daa10c218570d10600d38e7252155f68c84ebee0
Ataly <116371591+Atalyeu@users.noreply.github.com>
2024-11-14 17:48:52 +0200

    Add project files.

---
+++ PythonToC.cpp
@@ -0,0 +1,12 @@
+﻿// PythonToC.cpp : Defines the entry point for the application.
+//
+
+#include "PythonToC.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
